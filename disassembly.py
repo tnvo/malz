225a67bcdaa0beaddc39328f4bf9289b917e8ae6
T <tnvo@users.noreply.github.com>
2018-09-19 21:19:57 -0500

    initial commit

---
+++ disassembly.py
@@ -0,0 +1,17 @@
+"""Disassemble a PE file and dump the code"""
+
+#!/usr/bin/python
+
+import pefile
+from capstone import *
+
+pe = pefile.PE("your_file.exe")
+
+entry = pe.OPTIONAL_HEADER.AddressOfEntryPoint
+entry_addr = entry+pe.OPTIONAL_HEADER.ImageBase
+binary = pe.get_memory_mapped_image()[entry:entry+100]
+disassembler = Cs(CS_ARCH_X86, CS_MODE_32)
+
+# loop and disassemble the code
+for instr in disassembler.disasm(binary, entry_addr):
+    print "%s\t%s" %(instr.mnemonic, instr.op_str)
