3d33615c768df867db5624c23d21fa1856ba55ea
Thao Vo <tnvo@users.noreply.github.com>
2018-10-14 16:18:10 -0500

    Update pe_addr_info.py

--- pe_addr_info.py
+++ pe_addr_info.py
@@ -1,4 +1,13 @@
-"""Iterating through PE file's sections and print out base virtual memory addr, virtual size, and raw data size"""
+"""
+
+Iterating through PE file's sections and print out base virtual memory addr, virtual size, and raw data size
+
+Usage:
+Change "file.exe" to desired file
+$ python pe_addr_info.py
+
+
+"""
 
 #!/usr/bin/python
 
